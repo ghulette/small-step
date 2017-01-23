f500f97b0c76f37940a1a1fc79df2dd0275ccb25
Geoff Hulette <geoff@hulette.net>
2017-01-22 21:17:14 -0800

    Update

---
+++ Concurrent.v
@@ -0,0 +1,178 @@
+Require Import Coq.Arith.Arith.
+Require Import Coq.Arith.EqNat.
+Require Import Coq.omega.Omega.
+Require Import Maps.
+Require Import Imp.
+Require Import Smallstep.
+
+Inductive com : Type :=
+  | CSkip : com
+  | CAss : id -> aexp -> com
+  | CSeq : com -> com -> com
+  | CIf : bexp -> com -> com -> com
+  | CWhile : bexp -> com -> com
+  | CPar : com -> com -> com.
+
+Notation "'SKIP'" :=
+  CSkip.
+Notation "x '::=' a" :=
+  (CAss x a) (at level 60).
+Notation "c1 ;; c2" :=
+  (CSeq c1 c2) (at level 80, right associativity).
+Notation "'WHILE' b 'DO' c 'END'" :=
+  (CWhile b c) (at level 80, right associativity).
+Notation "'IFB' b 'THEN' c1 'ELSE' c2 'FI'" :=
+  (CIf b c1 c2) (at level 80, right associativity).
+Notation "'PAR' c1 'WITH' c2 'END'" :=
+  (CPar c1 c2) (at level 80, right associativity).
+
+Inductive cstep : (com * state)  -> (com * state) -> Prop :=
+  | CS_AssStep : forall st i a a',
+      a / st ==>a a' ->
+      (i ::= a) / st ==> (i ::= a') / st
+  | CS_Ass : forall st i n,
+      (i ::= (ANum n)) / st ==> SKIP / (t_update st i n)
+  | CS_SeqStep : forall st c1 c1' st' c2,
+      c1 / st ==> c1' / st' ->
+      (c1 ;; c2) / st ==> (c1' ;; c2) / st'
+  | CS_SeqFinish : forall st c2,
+      (SKIP ;; c2) / st ==> c2 / st
+  | CS_IfTrue : forall st c1 c2,
+      (IFB BTrue THEN c1 ELSE c2 FI) / st ==> c1 / st
+  | CS_IfFalse : forall st c1 c2,
+      (IFB BFalse THEN c1 ELSE c2 FI) / st ==> c2 / st
+  | CS_IfStep : forall st b b' c1 c2,
+      b /st ==>b b' ->
+          (IFB b THEN c1 ELSE c2 FI) / st
+      ==> (IFB b' THEN c1 ELSE c2 FI) / st
+  | CS_While : forall st b c1,
+          (WHILE b DO c1 END) / st
+      ==> (IFB b THEN (c1;; (WHILE b DO c1 END)) ELSE SKIP FI) / st
+  | CS_Par1 : forall st c1 c1' c2 st',
+      c1 / st ==> c1' / st' ->
+      (PAR c1 WITH c2 END) / st ==> (PAR c1' WITH c2 END) / st'
+  | CS_Par2 : forall st c1 c2 c2' st',
+      c2 / st ==> c2' / st' ->
+      (PAR c1 WITH c2 END) / st ==> (PAR c1 WITH c2' END) / st'
+  | CS_ParDone : forall st,
+      (PAR SKIP WITH SKIP END) / st ==> SKIP / st
+  where " t '/' st '==>' t' '/' st' " := (cstep (t,st) (t',st')).
+
+Definition cmultistep := multi cstep.
+
+Notation " t '/' st '==>*' t' '/' st' " :=
+   (multi cstep  (t,st) (t',st'))
+   (at level 40, st at level 39, t' at level 39).
+
+Definition par_loop : com :=
+  PAR
+    Y ::= ANum 1
+  WITH
+    WHILE BEq (AId Y) (ANum 0) DO
+      X ::= APlus (AId X) (ANum 1)
+    END
+  END.
+
+Example par_loop_example_0:
+  exists st',
+       par_loop / empty_state  ==>* SKIP / st'
+    /\ st' X = 0.
+Proof.
+  eapply ex_intro. split.
+  unfold par_loop.
+  eapply multi_step. apply CS_Par1.
+    apply CS_Ass.
+  eapply multi_step. apply CS_Par2. apply CS_While.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq1. apply AS_Id.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq. simpl.
+  eapply multi_step. apply CS_Par2. apply CS_IfFalse.
+  eapply multi_step. apply CS_ParDone.
+  eapply multi_refl.
+  reflexivity. Qed.
+
+Example par_loop_example_2:
+  exists st',
+       par_loop / empty_state ==>* SKIP / st'
+    /\ st' X = 2.
+Proof.
+  eapply ex_intro. split.
+  eapply multi_step. apply CS_Par2. apply CS_While.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq1. apply AS_Id.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq. simpl.
+  eapply multi_step. apply CS_Par2. apply CS_IfTrue.
+  eapply multi_step. apply CS_Par2. apply CS_SeqStep.
+    apply CS_AssStep. apply AS_Plus1. apply AS_Id.
+  eapply multi_step. apply CS_Par2. apply CS_SeqStep.
+    apply CS_AssStep. apply AS_Plus.
+  eapply multi_step. apply CS_Par2. apply CS_SeqStep.
+    apply CS_Ass.
+  eapply multi_step. apply CS_Par2. apply CS_SeqFinish.
+
+  eapply multi_step. apply CS_Par2. apply CS_While.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq1. apply AS_Id.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq. simpl.
+  eapply multi_step. apply CS_Par2. apply CS_IfTrue.
+  eapply multi_step. apply CS_Par2. apply CS_SeqStep.
+    apply CS_AssStep. apply AS_Plus1. apply AS_Id.
+  eapply multi_step. apply CS_Par2. apply CS_SeqStep.
+    apply CS_AssStep. apply AS_Plus.
+  eapply multi_step. apply CS_Par2. apply CS_SeqStep.
+    apply CS_Ass.
+
+  eapply multi_step. apply CS_Par1. apply CS_Ass.
+  eapply multi_step. apply CS_Par2. apply CS_SeqFinish.
+  eapply multi_step. apply CS_Par2. apply CS_While.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq1. apply AS_Id.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq. simpl.
+  eapply multi_step. apply CS_Par2. apply CS_IfFalse.
+  eapply multi_step. apply CS_ParDone.
+  eapply multi_refl.
+  reflexivity.
+Qed.
+
+Lemma par_body_n__Sn : forall n st,
+  st X = n /\ st Y = 0 ->
+  par_loop / st ==>* par_loop / (t_update st X (S n)).
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Lemma par_body_n : forall n st,
+  st X = 0 /\ st Y = 0 ->
+  exists st',
+    par_loop / st ==>*  par_loop / st' /\ st' X = n /\ st' Y = 0.
+Proof.
+  (* FILL IN HERE *) Admitted.
+
+Theorem par_loop_any_X:
+  forall n, exists st',
+    par_loop / empty_state ==>*  SKIP / st'
+    /\ st' X = n.
+Proof.
+  intros n.
+  destruct (par_body_n n empty_state).
+    split; unfold t_update; reflexivity.
+
+  rename x into st.
+  inversion H as [H' [HX HY]]; clear H.
+  exists (t_update st Y 1). split.
+  eapply multi_trans with (par_loop,st). apply H'.
+  eapply multi_step. apply CS_Par1. apply CS_Ass.
+  eapply multi_step. apply CS_Par2. apply CS_While.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq1. apply AS_Id. rewrite t_update_eq.
+  eapply multi_step. apply CS_Par2. apply CS_IfStep.
+    apply BS_Eq. simpl.
+  eapply multi_step. apply CS_Par2. apply CS_IfFalse.
+  eapply multi_step. apply CS_ParDone.
+  apply multi_refl.
+
+  rewrite t_update_neq. assumption. intro X; inversion X.
+Qed.
--- Imp.v
+++ Imp.v
@@ -6,11 +6,9 @@ Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Maps.
 
-
 Definition state := total_map nat.
 
-Definition empty_state : state :=
-  t_empty 0.
+Definition empty_state : state := t_empty 0.
 
 Inductive aexp : Type :=
   | ANum : nat -> aexp
@@ -68,9 +66,7 @@ Notation "'WHILE' b 'DO' c 'END'" :=
 Notation "'IFB' c1 'THEN' c2 'ELSE' c3 'FI'" :=
   (CIf c1 c2 c3) (at level 80, right associativity).
 
-
-(* ================================================================= *)
-(** ** Examples *)
+(* Examples *)
 
 Definition fact_in_coq : com :=
   Z ::= AId X;;
@@ -79,8 +75,6 @@ Definition fact_in_coq : com :=
     Y ::= AMult (AId Y) (AId Z);;
     Z ::= AMinus (AId Z) (ANum 1)
   END.
-
-(** Assignment: *)
 
 Definition plus2 : com :=
   X ::= (APlus (AId X) (ANum 2)).
@@ -92,9 +86,6 @@ Definition subtract_slowly_body : com :=
   Z ::= AMinus (AId Z) (ANum 1) ;;
   X ::= AMinus (AId X) (ANum 1).
 
-(* ----------------------------------------------------------------- *)
-(** *** Loops *)
-
 Definition subtract_slowly : com :=
   WHILE BNot (BEq (AId X) (ANum 0)) DO
     subtract_slowly_body
@@ -105,10 +96,7 @@ Definition subtract_3_from_5_slowly : com :=
   Z ::= ANum 5 ;;
   subtract_slowly.
 
-(* ----------------------------------------------------------------- *)
-(** *** An infinite loop: *)
-
-Definition loop : com :=
+Definition infinite_loop : com :=
   WHILE BTrue DO
     SKIP
   END.
@@ -182,11 +170,6 @@ Inductive ceval : com -> state -> state -> Prop :=
       (WHILE b DO c END) / st \\ st''
   where "c1 '/' st '\\' st'" := (ceval c1 st st').
 
-(** The cost of defining evaluation as a relation instead of a
-    function is that we now need to construct _proofs_ that some
-    program evaluates to some result state, rather than just letting
-    Coq's computation mechanism do it for us. *)
-
 Example ceval_example1:
     (X ::= ANum 2;;
      IFB BLe (AId X) (ANum 1)
@@ -196,54 +179,18 @@ Example ceval_example1:
    / empty_state
    \\ (t_update (t_update empty_state X 2) Z 4).
 Proof.
-  (* We must supply the intermediate state *)
   apply E_Seq with (t_update empty_state X 2).
-  - (* assignment command *)
     apply E_Ass. reflexivity.
-  - (* if command *)
     apply E_IfFalse.
       reflexivity.
-      apply E_Ass. reflexivity.  Qed.
+      apply E_Ass. reflexivity.
+Qed.
 
-(** **** Exercise: 2 stars (ceval_example2)  *)
 Example ceval_example2:
     (X ::= ANum 0;; Y ::= ANum 1;; Z ::= ANum 2) / empty_state \\
     (t_update (t_update (t_update empty_state X 0) Y 1) Z 2).
 Proof.
   (* FILL IN HERE *) Admitted.
-(** [] *)
-
-(** **** Exercise: 3 stars, advanced (pup_to_n)  *)
-(** Write an Imp program that sums the numbers from [1] to
-   [X] (inclusive: [1 + 2 + ... + X]) in the variable [Y].
-   Prove that this program executes as intended for [X] = [2]
-   (the latter is trickier than you might expect). *)
-
-Definition pup_to_n : com
-  (* REPLACE THIS LINE WITH   := _your_definition_ . *) . Admitted.
-
-Theorem pup_to_2_ceval :
-  pup_to_n / (t_update empty_state X 2) \\
-    t_update (t_update (t_update (t_update (t_update (t_update empty_state
-      X 2) Y 0) Y 2) X 1) Y 3) X 0.
-Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
-
-
-(* ================================================================= *)
-(** ** Determinism of Evaluation *)
-
-(** Changing from a computational to a relational definition of
-    evaluation is a good move because it allows us to escape from the
-    artificial requirement that evaluation should be a total function.
-    But it also raises a question: Is the second definition of
-    evaluation really a partial function?  Or is it possible that,
-    beginning from the same state [st], we could evaluate some command
-    [c] in different ways to reach two different output states [st']
-    and [st'']?
-
-    In fact, this cannot happen: [ceval] _is_ a partial function: *)
 
 Theorem ceval_deterministic: forall c st st1 st2,
      c / st \\ st1  ->
@@ -281,237 +228,29 @@ Proof.
       subst st'0.
       apply IHE1_2. assumption.  Qed.
 
-
-(* ################################################################# *)
-(** * Reasoning About Imp Programs *)
-
-(** We'll get much deeper into systematic techniques for reasoning
-    about Imp programs in the following chapters, but we can do quite
-    a bit just working with the bare definitions. *)
-
-(** This section explores some examples. *)
-
 Theorem plus2_spec : forall st n st',
   st X = n ->
   plus2 / st \\ st' ->
   st' X = n + 2.
 Proof.
   intros st n st' HX Heval.
-
-  (** Inverting [Heval] essentially forces Coq to expand one step of
-      the [ceval] computation -- in this case revealing that [st']
-      must be [st] extended with the new value of [X], since [plus2]
-      is an assignment *)
-
   inversion Heval. subst. clear Heval. simpl.
-  apply t_update_eq.  Qed.
+  apply t_update_eq.
+Qed.
 
-(** **** Exercise: 3 stars, recommended (XtimesYinZ_spec)  *)
-(** State and prove a specification of [XtimesYinZ]. *)
-
-(* FILL IN HERE *)
-(** [] *)
-
-(** **** Exercise: 3 stars, recommended (loop_never_stops)  *)
 Theorem loop_never_stops : forall st st',
-  ~(loop / st \\ st').
+  ~(infinite_loop / st \\ st').
 Proof.
-  intros st st' contra. unfold loop in contra.
+  intros st st' contra. unfold infinite_loop in contra.
   remember (WHILE BTrue DO SKIP END) as loopdef
            eqn:Heqloopdef.
-
-  (** Proceed by induction on the assumed derivation showing that
-      [loopdef] terminates.  Most of the cases are immediately
-      contradictory (and so can be solved in one step with
-      [inversion]). *)
-
   (* FILL IN HERE *) Admitted.
-(** [] *)
-
-(** **** Exercise: 3 stars (no_whilesR)  *)
-(** Consider the definition of the [no_whiles] boolean predicate below: *)
 
 Fixpoint no_whiles (c : com) : bool :=
   match c with
-  | SKIP =>
-      true
-  | _ ::= _ =>
-      true
-  | c1 ;; c2 =>
-      andb (no_whiles c1) (no_whiles c2)
-  | IFB _ THEN ct ELSE cf FI =>
-      andb (no_whiles ct) (no_whiles cf)
-  | WHILE _ DO _ END  =>
-      false
+  | SKIP => true
+  | _ ::= _ => true
+  | c1 ;; c2 => andb (no_whiles c1) (no_whiles c2)
+  | IFB _ THEN ct ELSE cf FI => andb (no_whiles ct) (no_whiles cf)
+  | WHILE _ DO _ END  => false
   end.
-
-(** This predicate yields [true] just on programs that have no while
-    loops.  Using [Inductive], write a property [no_whilesR] such that
-    [no_whilesR c] is provable exactly when [c] is a program with no
-    while loops.  Then prove its equivalence with [no_whiles]. *)
-
-Inductive no_whilesR: com -> Prop :=
- (* FILL IN HERE *)
-.
-
-Theorem no_whiles_eqv:
-   forall c, no_whiles c = true <-> no_whilesR c.
-Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
-
-(** **** Exercise: 4 stars (no_whiles_terminating)  *)
-(** Imp programs that don't involve while loops always terminate.
-    State and prove a theorem [no_whiles_terminating] that says this. *)
-(** Use either [no_whiles] or [no_whilesR], as you prefer. *)
-
-(* FILL IN HERE *)
-(** [] *)
-
-(* ################################################################# *)
-(** * Additional Exercises *)
-
-(** **** Exercise: 3 stars (stack_compiler)  *)
-(** HP Calculators, programming languages like Forth and Postscript,
-    and abstract machines like the Java Virtual Machine all evaluate
-    arithmetic expressions using a stack. For instance, the expression
-
-      (2*3)+(3*(4-2))
-
-   would be entered as
-
-      2 3 * 3 4 2 - * +
-
-   and evaluated like this (where we show the program being evaluated
-   on the right and the contents of the stack on the left):
-
-      []            |    2 3 * 3 4 2 - * +
-      [2]           |    3 * 3 4 2 - * +
-      [3, 2]        |    * 3 4 2 - * +
-      [6]           |    3 4 2 - * +
-      [3, 6]        |    4 2 - * +
-      [4, 3, 6]     |    2 - * +
-      [2, 4, 3, 6]  |    - * +
-      [2, 3, 6]     |    * +
-      [6, 6]        |    +
-      [12]          |
-
-  The task of this exercise is to write a small compiler that
-  translates [aexp]s into stack machine instructions.
-
-  The instruction set for our stack language will consist of the
-  following instructions:
-     - [SPush n]: Push the number [n] on the stack.
-     - [SLoad x]: Load the identifier [x] from the store and push it
-                  on the stack
-     - [SPlus]:   Pop the two top numbers from the stack, add them, and
-                  push the result onto the stack.
-     - [SMinus]:  Similar, but subtract.
-     - [SMult]:   Similar, but multiply. *)
-
-Inductive sinstr : Type :=
-| SPush : nat -> sinstr
-| SLoad : id -> sinstr
-| SPlus : sinstr
-| SMinus : sinstr
-| SMult : sinstr.
-
-(** Write a function to evaluate programs in the stack language. It
-    should take as input a state, a stack represented as a list of
-    numbers (top stack item is the head of the list), and a program
-    represented as a list of instructions, and it should return the
-    stack after executing the program.  Test your function on the
-    examples below.
-
-    Note that the specification leaves unspecified what to do when
-    encountering an [SPlus], [SMinus], or [SMult] instruction if the
-    stack contains less than two elements.  In a sense, it is
-    immaterial what we do, since our compiler will never emit such a
-    malformed program. *)
-
-Fixpoint s_execute (st : state) (stack : list nat)
-                   (prog : list sinstr)
-                 : list nat
-  (* REPLACE THIS LINE WITH   := _your_definition_ . *) . Admitted.
-
-Example s_execute1 :
-     s_execute empty_state []
-       [SPush 5; SPush 3; SPush 1; SMinus]
-   = [2; 5].
-(* FILL IN HERE *) Admitted.
-
-Example s_execute2 :
-     s_execute (t_update empty_state X 3) [3;4]
-       [SPush 4; SLoad X; SMult; SPlus]
-   = [15; 4].
-(* FILL IN HERE *) Admitted.
-
-(** Next, write a function that compiles an [aexp] into a stack
-    machine program. The effect of running the program should be the
-    same as pushing the value of the expression on the stack. *)
-
-Fixpoint s_compile (e : aexp) : list sinstr
-  (* REPLACE THIS LINE WITH   := _your_definition_ . *) . Admitted.
-
-(** After you've defined [s_compile], prove the following to test
-    that it works. *)
-
-Example s_compile1 :
-    s_compile (AMinus (AId X) (AMult (ANum 2) (AId Y)))
-  = [SLoad X; SPush 2; SLoad Y; SMult; SMinus].
-(* FILL IN HERE *) Admitted.
-(** [] *)
-
-(** **** Exercise: 4 stars, advanced (stack_compiler_correct)  *)
-(** Now we'll prove the correctness of the compiler implemented in the
-    previous exercise.  Remember that the specification left
-    unspecified what to do when encountering an [SPlus], [SMinus], or
-    [SMult] instruction if the stack contains less than two
-    elements.  (In order to make your correctness proof easier you
-    might find it helpful to go back and change your implementation!)
-
-    Prove the following theorem.  You will need to start by stating a
-    more general lemma to get a usable induction hypothesis; the main
-    theorem will then be a simple corollary of this lemma. *)
-
-
-Theorem s_compile_correct : forall (st : state) (e : aexp),
-  s_execute st [] (s_compile e) = [ aeval st e ].
-Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
-
-
-(** **** Exercise: 3 stars, optional (short_circuit)  *)
-(** Most modern programming languages use a "short-circuit" evaluation
-    rule for boolean [and]: to evaluate [BAnd b1 b2], first evaluate
-    [b1].  If it evaluates to [false], then the entire [BAnd]
-    expression evaluates to [false] immediately, without evaluating
-    [b2].  Otherwise, [b2] is evaluated to determine the result of the
-    [BAnd] expression.
-
-    Write an alternate version of [beval] that performs short-circuit
-    evaluation of [BAnd] in this manner, and prove that it is
-    equivalent to [beval]. *)
-
-(* FILL IN HERE *)
-(** [] *)
-
-(** **** Exercise: 4 stars, optional (add_for_loop)  *)
-(** Add C-style [for] loops to the language of commands, update the
-    [ceval] definition to define the semantics of [for] loops, and add
-    cases for [for] loops as needed so that all the proofs in this file
-    are accepted by Coq.
-
-    A [for] loop should be parameterized by (a) a statement executed
-    initially, (b) a test that is run on each iteration of the loop to
-    determine whether the loop should continue, (c) a statement
-    executed at the end of each loop iteration, and (d) a statement
-    that makes up the body of the loop.  (You don't need to worry
-    about making up a concrete Notation for [for] loops, but feel free
-    to play with this too if you like.) *)
-
-(* FILL IN HERE *)
-(** [] *)
-
-(* $Date: 2016-07-14 17:02:35 -0400 (Thu, 14 Jul 2016) $ *)
